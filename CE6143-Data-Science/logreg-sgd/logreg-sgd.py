24047ea1f4806146e003667aa34b5c72600070a0
Hsu <edittye379@gmail.com>
2017-05-23 11:29:46 +0800

    :books:

--- CE6143-Data-Science/logreg-sgd/logreg-sgd.py
+++ CE6143-Data-Science/logreg-sgd/logreg-sgd.py
@@ -69,6 +69,7 @@ def logreg_sgd(X, y, alpha = .001, iters = 100000, eps=1e-4):
         convert = lambda n : 1e-10 if n < 0.5 else (1 - 1e-10)
         prediction = map(convert, yHat)
         
+        # http://www.codelast.com/%E5%8E%9F%E5%88%9B-%E7%94%A8%E4%BA%BA%E8%AF%9D%E8%A7%A3%E9%87%8A%E6%9C%BA%E5%99%A8%E5%AD%A6%E4%B9%A0%E4%B8%AD%E7%9A%84logistic-regression%EF%BC%88%E9%80%BB%E8%BE%91%E5%9B%9E%E5%BD%92%EF%BC%89/
         loss = cross_entropy(y, prediction)
         if ((prev_loss - loss) < eps):
             # print('iter: %d break' % i)
